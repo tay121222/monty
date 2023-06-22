157a83bd6e29d842eed3e084349e5aff4af25fc4
Enoch Taylor <tay121222@gmail.com>
2023-06-22 21:52:57 +0000

    Modification to swap function

--- stack_functions1.c
+++ stack_functions1.c
@@ -14,7 +14,9 @@ void swap(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	i = pop(stack);
-	insert_dnodeint_at_index(stack, 1, i);
+	i = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = i;
+	/*insert_dnodeint_at_index(stack, 1, i);*/
 }
 
